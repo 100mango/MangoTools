9f8410775d3bece53431141d98c6847da1b0b622
100mango <100mango@users.noreply.github.com>
2015-02-02 21:27:04 +0800

    add string tools

---
+++ NSString+Tools.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Tools.h
+//  Trans
+//
+//  Created by Mango on 15/2/2.
+//  Copyright (c) 2015年 Mango. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Tools)
+
+- (BOOL)isMobileNumber:(NSString *)mobileNum
+
+
+@end
