a2360522d9dbe46e594f1de6fad39f649adbe38a
MangoFang <100mango@gmail.com>
2016-02-29 20:54:11 +0800

    mark as public. add UITableView,UITableViewCell,UICollectionView,UICollectionViewCell extension

--- Swift/UIView-Extension.swift
+++ Swift/UIView-Extension.swift
@@ -8,50 +8,78 @@
 
 import UIKit
 
-extension UIView{
+//MARK: UIView
+
+public extension UIView{
     
-    var width: CGFloat {
+    public var width: CGFloat {
         get { return self.frame.size.width }
         set { self.frame.size.width = newValue }
     }
     
-    var height: CGFloat {
+    public var height: CGFloat {
         get { return self.frame.size.height }
         set { self.frame.size.height = newValue }
     }
     
-    var top: CGFloat {
+    public var top: CGFloat {
         get { return self.frame.origin.y }
         set { self.frame.origin.y = newValue }
     }
-    var right: CGFloat {
+    public var right: CGFloat {
         get { return self.frame.origin.x + self.width }
         set { self.frame.origin.x = newValue - self.width }
     }
-    var bottom: CGFloat {
+    public var bottom: CGFloat {
         get { return self.frame.origin.y + self.height }
         set { self.frame.origin.y = newValue - self.height }
     }
-    var left: CGFloat {
+    public var left: CGFloat {
         get { return self.frame.origin.x }
         set { self.frame.origin.x = newValue }
     }
     
-    var centerX: CGFloat{
+    public var centerX: CGFloat{
         get { return self.center.x }
         set { self.center = CGPoint(x: newValue,y: self.centerY) }
     }
-    var centerY: CGFloat {
+    public var centerY: CGFloat {
         get { return self.center.y }
         set { self.center = CGPoint(x: self.centerX,y: newValue) }
     }
     
-    var origin: CGPoint {
+    public var origin: CGPoint {
         set { self.frame.origin = newValue }
         get { return self.frame.origin }
     }
-    var size: CGSize {
+    public var size: CGSize {
         set { self.frame.size = newValue }
         get { return self.frame.size }
+    }
+}
+
+//MARK: UITableView,UITableViewCell,UICollectionView,UICollectionViewCell
+
+public extension UITableView{
+    public func registerClass(cellType:UITableViewCell.Type){
+        registerClass(cellType, forCellReuseIdentifier: cellType.defaultReuseIdentifier)
+    }
+}
+
+public extension UITableViewCell{
+    public static var defaultReuseIdentifier:String{
+        return String(self)
+    }
+}
+
+public extension UICollectionView{
+    public func registerClass(cellType:UICollectionViewCell.Type){
+        registerClass(cellType, forCellWithReuseIdentifier: cellType.defaultReuseIdentifier)
+    }
+}
+
+public extension UICollectionViewCell{
+    public static var defaultReuseIdentifier:String{
+        return String(self)
     }
 }
